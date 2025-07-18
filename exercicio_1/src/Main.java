9e6bbe73ec467ea849df9bd30a2015fa56833807
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-18 16:12:33 -0300

    Feat: exercicios finalizado

--- exercicio_1/src/Main.java
+++ exercicio_1/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,18 +7,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-
-        Scanner sc = new Scanner(System.in);
-
-        int nums[];
-
-
-
-        sc.close();
-
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("Exemplo 1: nums = " + Arrays.toString(nums1) + ", target = " + target1 + " -> Saída: " + Arrays.toString(result1));
+        // --- Exemplo 2 ---
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("Exemplo 2: nums = " + Arrays.toString(nums2) + ", target = " + target2 + " -> Saída: " + Arrays.toString(result2));
+        // --- Exemplo 3 ---
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = twoSum(nums3, target3);
+        System.out.println("Exemplo 3: nums = " + Arrays.toString(nums3) + ", target = " + target3 + " -> Saída: " + Arrays.toString(result3));
     }
 
     public static int[] twoSum(int[] nums, int target){
-        for (int i = 0; i < nums.length; i++)
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
     }
 }
