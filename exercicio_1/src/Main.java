1cf9a0ae7df5c37d888edc2f97dd401b602e20e0
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-18 15:37:49 -0300

    Feat: inicializando exercicio de lógica

---
+++ exercicio_1/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner sc = new Scanner(System.in);
+
+        int nums[];
+
+
+
+        sc.close();
+
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+        for (int i = 0; i < nums.length; i++)
+    }
+}
