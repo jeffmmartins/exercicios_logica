885932831af4fb544d5a371e47e750860b7184ed
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-18 16:35:57 -0300

    Feat: Exercicio finalizado

--- exercicio_2/src/Main.java
+++ exercicio_2/src/Main.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.Source;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,6 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int nums = sc.nextInt();
+
+        String num = String.valueOf(nums);
+
+        String sInvertida = new StringBuilder(num).reverse().toString();
+
+        boolean result = num.equals(sInvertida);
+
+        System.out.println(result);
 
 
         sc.close();
