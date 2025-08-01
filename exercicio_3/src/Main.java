23e96c21c5b64a51256d0815b7bf34ba8e4a1826
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-01 11:17:44 -0300

    Feat: exercicio de lógica do leetcode finzalizado

---
+++ exercicio_3/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
