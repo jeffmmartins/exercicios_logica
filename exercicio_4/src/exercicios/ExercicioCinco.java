486edaf61ed20bb34be80cc5d76f911c1ddc2d72
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-07 16:00:35 -0300

    Feat: exercicio 4 do leetcode finalizado

---
+++ exercicio_4/src/exercicios/ExercicioCinco.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class ExercicioCinco {
+    public int searchInsert(int[] nums, int target) {
+        class Solution {
+            public int searchInsert(int[] nums, int target) {
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] >= target) {
+                        return i;
+                    }
+                }
+                return nums.length;
+            }
+        }
+    }
+}
+
