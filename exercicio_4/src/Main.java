e50462963202f023a7f3babe62cc13a6d5cde01a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-09 21:24:48 -0300

    Fix: corrigindo bug no exercicio de logica

--- exercicio_4/src/Main.java
+++ exercicio_4/src/Main.java
@@ -1,21 +1,13 @@
+import exercicios.ExercicioCinco;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        public int removeElement(int[] nums, int val) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Digite os números");
-            for(int i = 0; i <= nums.length; i++){
-                if (nums[i] != val) {
-                    nums = new int[]{nums[i]};
-                }
-            }
+        ExercicioCinco ex5 = new ExercicioCinco();
+        ex5.searchInsert([1,2,3,4],5);
 
-            return nums.length;
-
-            sc.close();
-        }
     }
 }
