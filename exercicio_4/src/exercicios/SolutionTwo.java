cec4695f184166e58c61f3753d41812481f6c4ce
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-19 09:05:26 -0300

    Feat: exercicios de lógica do leetcode

---
+++ exercicio_4/src/exercicios/SolutionTwo.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class SolutionTwo {
+
+    public int strStr(String haystack, String needle) {
+       if (needle.equals(haystack)){
+           return 0;
+       } else {
+           return -1;
+       }
+    }
+}
+
